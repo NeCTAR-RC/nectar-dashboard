04c097ecae01de41bd2fa6c840e2f6185ee02921
Sam Morrison <sorrison@gmail.com>
2015-11-12 13:13:35 +1100

    Remove overrides not needed, use new method
    Change-Id: I4f642898619867f53379c6c7ee77849d1fafae55

--- nectar_dashboard/overrides.py
+++ nectar_dashboard/overrides.py
@@ -1,5 +1,3 @@
-import horizon
-
 # Disable Floating IPs
 from openstack_dashboard.dashboards.project.access_and_security import tabs
 from openstack_dashboard.dashboards.project.instances import tables
@@ -10,18 +8,3 @@ tabs.FloatingIPsTab.allowed = NO
 tables.AssociateIP.allowed = NO
 tables.SimpleAssociateIP.allowed = NO
 tables.SimpleDisassociateIP.allowed = NO
-
-
-project = horizon.get_dashboard("project")
-network_panel = project.get_panel("networks")
-network_topology_panel = project.get_panel("network_topology")
-project.unregister(network_panel.__class__)
-project.unregister(network_topology_panel.__class__)
-
-from openstack_dashboard.dashboards.identity.projects import tables
-from openstack_dashboard.dashboards.identity.projects import views
-
-#class MyProjectsTable(
-#identity = horizon.get_dashboard("identity")
-#projects_panel = identity.get_panel("projects")
-#identity.unregister(projects_panel.__class__)
