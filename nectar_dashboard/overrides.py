b28aa49f72a10cc0eac89d360a558f1cabb733f3
Sam Morrison <sorrison@gmail.com>
2015-11-16 10:13:48 +1100

    remove SetNetwork tab from create instance workflow
    Change-Id: I81773512b69e0799539994a16b7dca0ea6ac66f2

--- nectar_dashboard/overrides.py
+++ nectar_dashboard/overrides.py
@@ -1,10 +1,31 @@
 # Disable Floating IPs
 from openstack_dashboard.dashboards.project.access_and_security import tabs
 from openstack_dashboard.dashboards.project.instances import tables
+from openstack_dashboard.dashboards.project.instances import views
+from openstack_dashboard.dashboards.project.instances.workflows.create_instance import LaunchInstance
+from openstack_dashboard.dashboards.project.instances.workflows.create_instance import SelectProjectUser
+from openstack_dashboard.dashboards.project.instances.workflows.create_instance import SetInstanceDetails
+from openstack_dashboard.dashboards.project.instances.workflows.create_instance import SetAccessControls
+from openstack_dashboard.dashboards.project.instances.workflows.create_instance import CellSelection
+from openstack_dashboard.dashboards.project.instances.workflows.create_instance import PostCreationStep
+from openstack_dashboard.dashboards.project.instances.workflows.create_instance import SetAdvanced
 
+# Disable Floating IPs
 NO = lambda *x: False
 
 tabs.FloatingIPsTab.allowed = NO
 tables.AssociateIP.allowed = NO
 tables.SimpleAssociateIP.allowed = NO
 tables.SimpleDisassociateIP.allowed = NO
+
+
+# Remove SetNetwork tab from launch instance
+class NeCTARLaunchInstance(LaunchInstance):
+    default_steps = (SelectProjectUser,
+                     SetInstanceDetails,
+                     SetAccessControls,
+                     CellSelection,
+                     PostCreationStep,
+                     SetAdvanced)
+
+views.LaunchInstanceView.workflow_class = NeCTARLaunchInstance
