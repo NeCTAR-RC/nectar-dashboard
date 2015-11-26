fd73a6c0901e9a049887e020e987a846eb6e60ea
Sam Morrison <sorrison@gmail.com>
2015-11-26 12:32:36 +1100

    Don't specify rest_framework under alloctions
    Change-Id: I302a5069c18769728182a725b244faf20130b3ff

--- nectar_dashboard/rcallocation/request/urls.py
+++ nectar_dashboard/rcallocation/request/urls.py
@@ -1,16 +1,8 @@
-from django.conf.urls import url, patterns, include
+from django.conf.urls import url, patterns
 from .views import AllocationCreateView
-
-from nectar_dashboard.rcallocation import api
-from rest_framework import routers
-
-router = routers.DefaultRouter()
-router.register(r'allocations', api.AllocationViewSet)
-router.register(r'quotas', api.QuotaViewSet)
 
 
 urlpatterns = patterns(
     'nectar_dashboard.rcallocation.request',
     url(r'^$', AllocationCreateView.as_view(), name='request'),
-    url(r'^api/', include(router.urls)),
 )
