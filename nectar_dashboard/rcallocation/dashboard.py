28aa52d7365cef20acce9d35ac9fd6283fc9bbc5
Melvin Luong <melvin.luong@gmail.com>
2016-05-26 10:22:54 +1000

    Added new 'CRAMS' panel to the 'Allocation' dashboard that will redirect to the CRAMS portal for future allocation requests.
    'New Request' and 'My Requests' panels have been set to be
    viewable by admin only.
    
    Change-Id: I0e3a9159f864201c3423b73f50c4d4f7b4242d71

--- nectar_dashboard/rcallocation/dashboard.py
+++ nectar_dashboard/rcallocation/dashboard.py
@@ -6,7 +6,9 @@ import horizon
 class AllocationsDashboard(horizon.Dashboard):
     name = _("Allocations")
     slug = "allocation"
-    panels = ('request', 'requests', 'approved_requests', 'user_requests')
+    panels = ('request', 'requests', 'approved_requests', 'user_requests',
+              'crams_requests',)
     default_panel = 'request'
+
 
 horizon.register(AllocationsDashboard)
