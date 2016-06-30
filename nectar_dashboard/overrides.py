75bbe7bcc89aab29e470490b066c663daecc6234
Sam Morrison <sorrison@gmail.com>
2016-07-01 09:38:12 +1000

    Don't disable floating IPs
    Change-Id: Ie07113b52089a9b58fff761b671a0f5baaf26011

--- nectar_dashboard/overrides.py
+++ nectar_dashboard/overrides.py
@@ -1,13 +1,1 @@
-# Disable Floating IPs
-from openstack_dashboard.dashboards.project.access_and_security import tabs
-from openstack_dashboard.dashboards.project.instances import tables
-
-# Disable Floating IPs
-NO = lambda *x: False
-
-tabs.FloatingIPsTab.allowed = NO
-tables.AssociateIP.allowed = NO
-tables.SimpleAssociateIP.allowed = NO
-tables.SimpleDisassociateIP.allowed = NO
-
-
+# Currently no overrides
