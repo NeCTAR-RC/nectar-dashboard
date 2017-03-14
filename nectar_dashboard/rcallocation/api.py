db348fe20745ff37ba70f7716d6761ee99acacef
Sam Morrison <sorrison@gmail.com>
2017-03-15 09:34:29 +1100

    Filter on more attributes for allocations
    Change-Id: I6449b44623ec2b6b52e253e702dd5403d78fd312

--- nectar_dashboard/rcallocation/api.py
+++ nectar_dashboard/rcallocation/api.py
@@ -11,7 +11,7 @@ class AllocationSerializer(serializers.ModelSerializer):
 class AllocationViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = models.AllocationRequest.objects.all()
     serializer_class = AllocationSerializer
-    filter_fields = ('status',)
+    filter_fields = ('status', 'parent_request_id', 'tenant_uuid', 'tenant_name')
 
 
 class QuotaSerializer(serializers.ModelSerializer):
