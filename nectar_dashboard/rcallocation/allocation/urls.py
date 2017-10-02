fe0223a7b38a03d5523a95c4339a99f7b88d45dd
Sam Morrison <sorrison@gmail.com>
2017-10-02 13:40:22 +1100

    Import modules not classes
    Change-Id: I08570d122ac73accf0162530722f15578754d2c8

--- nectar_dashboard/rcallocation/allocation/urls.py
+++ nectar_dashboard/rcallocation/allocation/urls.py
@@ -1,27 +1,21 @@
 from django.conf.urls import url, patterns
-from nectar_dashboard.rcallocation.views import (
-    AllocationDetailView,
-    AllocationsListView,
-    AllocationHistoryView,
-)
-from nectar_dashboard.rcallocation.allocation.views import (
-    AllocationUpdateView,
-    AllocationApproveView,
-    AllocationRejectView,
-)
+
+from nectar_dashboard.rcallocation import views
+from nectar_dashboard.rcallocation.allocation import views as allocation_views
+
 
 urlpatterns = patterns('nectar_dashboard.rcallocation.allocation',
-    url(r'^$', AllocationsListView.as_view(), name='allocation_requests'),
-    url(r'^view/(?P<pk>\d+)/$', AllocationDetailView.as_view(),
+    url(r'^$', views.AllocationsListView.as_view(), name='allocation_requests'),
+    url(r'^view/(?P<pk>\d+)/$', views.AllocationDetailView.as_view(),
         name='allocation_view'),
-    url(r'^view/(?P<pk>\d+)/history$', AllocationHistoryView.as_view(),
+    url(r'^view/(?P<pk>\d+)/history$', views.AllocationHistoryView.as_view(),
         name='allocation_history'),
-    url(r'^edit_request/(?P<pk>\d+)/$', AllocationUpdateView.as_view(),
+    url(r'^edit_request/(?P<pk>\d+)/$', allocation_views.AllocationUpdateView.as_view(),
         name='edit_request'),
-    url(r'^approve_request/(?P<pk>\d+)/$', AllocationApproveView.as_view(),
+    url(r'^approve_request/(?P<pk>\d+)/$', allocation_views.AllocationApproveView.as_view(),
         name='approve_request'),
-    url(r'^reject_request/(?P<pk>\d+)/$', AllocationRejectView.as_view(),
+    url(r'^reject_request/(?P<pk>\d+)/$', allocation_views.AllocationRejectView.as_view(),
         name='reject_request'),
-    url(r'^approve_change_request/(?P<pk>\d+)/$', AllocationApproveView.as_view(),
+    url(r'^approve_change_request/(?P<pk>\d+)/$', allocation_views.AllocationApproveView.as_view(),
         name='approve_change_request'),
 )
