86604d61dc97a9e8d1ef65a18eaa1aeef7e84d4e
Sam Morrison <sorrison@gmail.com>
2017-10-02 13:40:22 +1100

    Import modules not classes
    Change-Id: I08570d122ac73accf0162530722f15578754d2c8

--- nectar_dashboard/rcallocation/allocation/views.py
+++ nectar_dashboard/rcallocation/allocation/views.py
@@ -1,47 +1,46 @@
 from django.forms.models import inlineformset_factory
 
-from nectar_dashboard.rcallocation.models import AllocationRequest, Quota, \
-    ChiefInvestigator, Institution, Publication, Grant
-from nectar_dashboard.rcallocation.forms import AllocationRequestForm, \
-    ChiefInvestigatorForm, InstitutionForm, PublicationForm, GrantForm
-from nectar_dashboard.rcallocation.views import BaseAllocationView
-from .forms import (
-    AllocationApproveForm,
-    AllocationRejectForm,
-    QuotaForm,
-)
+from nectar_dashboard.rcallocation import models
+from nectar_dashboard.rcallocation import forms
+from nectar_dashboard.rcallocation import views
+from nectar_dashboard.rcallocation.allocation import forms as allocation_forms
 
 
-class AllocationUpdateView(BaseAllocationView):
+class AllocationUpdateView(views.BaseAllocationView):
     template_name = "rcallocation/allocationrequest_update.html"
-    model = AllocationRequest
-    form_class = AllocationRequestForm
+    model = models.AllocationRequest
+    form_class = forms.AllocationRequestForm
     success_url = "../../"
 
 
-class AllocationApproveView(BaseAllocationView):
+class AllocationApproveView(views.BaseAllocationView):
     template_name = "rcallocation/allocationrequest_approve.html"
-    model = AllocationRequest
-    form_class = AllocationApproveForm
+    model = models.AllocationRequest
+    form_class = allocation_forms.AllocationApproveForm
     success_url = "../../"
     formset_quota_class = inlineformset_factory(
-        AllocationRequest, Quota, form=QuotaForm, extra=0, can_delete=False)
+        models.AllocationRequest, models.Quota,
+        form=allocation_forms.QuotaForm, extra=0, can_delete=False)
     formset_investigator_class = inlineformset_factory(
-        AllocationRequest, ChiefInvestigator, form=ChiefInvestigatorForm,
+        models.AllocationRequest, models.ChiefInvestigator,
+        form=forms.ChiefInvestigatorForm,
         extra=0, can_delete=False)
     formset_institution_class = inlineformset_factory(
-        AllocationRequest, Institution, form=InstitutionForm, extra=0,
+        models.AllocationRequest, models.Institution,
+        form=forms.InstitutionForm, extra=0,
         can_delete=False)
     formset_publication_class = inlineformset_factory(
-        AllocationRequest, Publication, form=PublicationForm, extra=0,
+        models.AllocationRequest, models.Publication,
+        form=forms.PublicationForm, extra=0,
         can_delete=False)
     formset_grant_class = inlineformset_factory(
-        AllocationRequest, Grant, form=GrantForm, extra=0, can_delete=False)
+        models.AllocationRequest, models.Grant,
+        form=forms.GrantForm, extra=0, can_delete=False)
 
 
-class AllocationRejectView(BaseAllocationView):
+class AllocationRejectView(views.BaseAllocationView):
     template_name = "rcallocation/allocationrequest_reject.html"
-    model = AllocationRequest
-    form_class = AllocationRejectForm
+    model = models.AllocationRequest
+    form_class = allocation_forms.AllocationRejectForm
     formset_quota_class = None
     success_url = "../../"
