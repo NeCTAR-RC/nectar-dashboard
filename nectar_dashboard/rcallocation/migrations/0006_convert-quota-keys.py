61c8115120542d18295649d3311cb07baf04ce45
Sam Morrison <sorrison@gmail.com>
2017-10-06 16:31:16 +1100

    Refactor how quotas work and are displayed
    Change-Id: Ie742fd7faa58dd0cfe2360f8e8262d007b7241f2

---
+++ nectar_dashboard/rcallocation/migrations/0006_convert-quota-keys.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+def convert_resource(apps, schema_editor):
+    Resource = apps.get_model('rcallocation', 'Resource')
+    Quota = apps.get_model('rcallocation', 'Quota')
+
+    resource_cache = {}
+    for quota in Quota.objects.all():
+        if '.' in quota.resource:
+            orig_resource = quota.resource.split('.')[1]
+        else:
+            orig_resource = quota.resource
+        if orig_resource in resource_cache:
+            resource_id = resource_cache[orig_resource]
+        else:
+            resource = Resource.objects.get(quota_name=orig_resource)
+            resource_cache[orig_resource] = resource.id
+            resource_id = resource.id
+
+        quota.resource_id = resource_id
+        quota.save()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('rcallocation', '0005_add-service-type-zone-resource-tables'),
+    ]
+
+    operations = [
+        migrations.AlterUniqueTogether(
+            name='quota',
+            unique_together=(),
+        ),
+        migrations.AddField(
+            model_name='quota',
+            name='resource_id',
+            field=models.IntegerField(),
+        ),
+        migrations.RunPython(convert_resource),
+        migrations.RemoveField(
+            model_name='quota',
+            name='resource',
+        ),
+        migrations.RenameField(
+            model_name='quota',
+            old_name='resource_id',
+            new_name='resource',
+        ),
+        migrations.AlterField(
+            model_name='quota',
+            name='resource',
+            field=models.ForeignKey(to='rcallocation.Resource'),
+        ),
+        migrations.AlterField(
+            model_name='quota',
+            name='zone',
+            field=models.ForeignKey(to='rcallocation.Zone'),
+        ),
+        migrations.AlterField(
+            model_name='zone',
+            name='display_name',
+            field=models.CharField(max_length=64),
+        ),
+        migrations.AlterUniqueTogether(
+            name='quota',
+            unique_together=set([('allocation', 'resource', 'zone')]),
+        ),
+        migrations.RemoveField(
+            model_name='quota',
+            name='units',
+        ),
+    ]
