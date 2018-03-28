e39b46a2b819d09be7adec96e7ad2dec04c3e54a
Sam Morrison <sorrison@gmail.com>
2018-03-28 14:19:15 +1100

    Panel configuration and disabling
    Change-Id: I9f026767e5e871f1c4f1ec6c6d26656c037f9970

---
+++ enabled/_40_disable_change_password.py
@@ -0,0 +1,7 @@
+# The slug of the panel to be added to HORIZON_CONFIG. Required.
+PANEL = 'password'
+# The slug of the dashboard the PANEL associated with. Required.
+PANEL_DASHBOARD = 'settings'
+
+# If set to True, the panel will be removed from PANEL_DASHBOARD/PANEL_GROUP.
+REMOVE_PANEL = True
