80dbf9fe27e14f3680a713353cb183653cbad0c9
Sam Morrison <sorrison@gmail.com>
2018-03-28 13:46:37 +1100

    Don't display volume type in create volume form
    Change-Id: Iddf408f3fb47f1d8e2111e172195d05d759b9777

--- nectar_dashboard/overrides.py
+++ nectar_dashboard/overrides.py
@@ -1,1 +1,19 @@
 # Currently no overrides
+
+from django.utils.translation import ugettext_lazy as _
+
+from horizon import forms
+
+from openstack_dashboard.dashboards.project.volumes import views as volume_views # noqa
+from openstack_dashboard.dashboards.project.volumes import forms as volume_forms # noqa
+
+
+class NectarVolumeCreateForm(volume_forms.CreateForm):
+
+    type = forms.ChoiceField(
+        label=_("Type"),
+        required=False,
+        widget=forms.HiddenInput())
+
+
+volume_views.CreateView.form_class = NectarVolumeCreateForm
