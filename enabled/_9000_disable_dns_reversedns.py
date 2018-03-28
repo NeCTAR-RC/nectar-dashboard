4b35e943344d4bf281f576ba98ab40e2260df103
Sam Morrison <sorrison@gmail.com>
2018-03-29 09:59:08 +1100

    Disable reverse DNS panel
    Change-Id: Ib99a8f6ca64fdab43284d53eadbde3eecc723ea5

---
+++ enabled/_9000_disable_dns_reversedns.py
@@ -0,0 +1,8 @@
+# The slug of the panel to be added to HORIZON_CONFIG. Required.
+PANEL = 'reverse_dns'
+# The slug of the dashboard the PANEL associated with. Required.
+PANEL_DASHBOARD = 'project'
+
+PANEL_GROUP = 'dns'
+# If set to True, the panel will be removed from PANEL_DASHBOARD/PANEL_GROUP.
+REMOVE_PANEL = True
