46be48b9ec09581d36872d99bdc24c929652968c
Sam Morrison <sorrison@gmail.com>
2018-03-29 12:22:34 +1100

    Hide volume_type field on trove create instance
    Change-Id: Ibf9b52effe948dd5643cd86a9d88283da3df80d9

--- nectar_dashboard/overrides.py
+++ nectar_dashboard/overrides.py
@@ -1,8 +1,9 @@
 # Currently no overrides
 
 from django.utils.translation import ugettext_lazy as _
-
 from horizon import forms
+
+from trove_dashboard.content.databases.workflows import create_instance
 
 from openstack_dashboard.dashboards.project.volumes import views as volume_views # noqa
 from openstack_dashboard.dashboards.project.volumes import forms as volume_forms # noqa
@@ -17,3 +18,17 @@ class NectarVolumeCreateForm(volume_forms.CreateForm):
 
 
 volume_views.CreateView.form_class = NectarVolumeCreateForm
+
+
+class TroveSetInstanceDetailsAction(create_instance.SetInstanceDetailsAction):
+    volume_type = forms.ChoiceField(
+        label=_("Volume Type"),
+        required=False,
+        widget=forms.HiddenInput())
+
+    class Meta(object):
+        name = _("Details")
+        help_text_template = "project/databases/_launch_details_help.html"
+
+
+create_instance.SetInstanceDetails.action_class = TroveSetInstanceDetailsAction
