0646a45a8780c59c59f9941c25aa703e35c08b99
Sam Morrison <sorrison@gmail.com>
2018-03-29 16:48:57 +1100

    Fix volume form when specifying from snapshot or volume
    Change-Id: Idee44e2348f7a89479e7cf6932b41ef41b6019ff

--- nectar_dashboard/overrides.py
+++ nectar_dashboard/overrides.py
@@ -16,6 +16,57 @@ class NectarVolumeCreateForm(volume_forms.CreateForm):
         required=False,
         widget=forms.HiddenInput())
 
+    def prepare_source_fields_if_snapshot_specified(self, request):
+        try:
+            snapshot = self.get_snapshot(request,
+                                         request.GET["snapshot_id"])
+            self.fields['name'].initial = snapshot.name
+            self.fields['size'].initial = snapshot.size
+            self.fields['snapshot_source'].choices = ((snapshot.id,
+                                                       snapshot),)
+            try:
+                # Set the volume type from the original volume
+                orig_volume = cinder.volume_get(request,
+                                                snapshot.volume_id)
+            except Exception:
+                pass
+            self.fields['size'].help_text = (
+                _('Volume size must be equal to or greater than the '
+                  'snapshot size (%sGiB)') % snapshot.size)
+            self.fields['type'].widget = forms.widgets.HiddenInput()
+            del self.fields['image_source']
+            del self.fields['volume_source']
+            del self.fields['volume_source_type']
+            del self.fields['availability_zone']
+            
+        except Exception:
+            exceptions.handle(request,
+                              _('Unable to load the specified snapshot.'))
+
+    def prepare_source_fields_if_volume_specified(self, request):
+        self.fields['availability_zone'].choices = \
+                                                   availability_zones(request)
+        volume = None
+        try:
+            volume = self.get_volume(request, request.GET["volume_id"])
+        except Exception:
+            msg = _('Unable to load the specified volume. %s')
+            exceptions.handle(request, msg % request.GET['volume_id'])
+            
+        if volume is not None:
+            self.fields['name'].initial = volume.name
+            self.fields['description'].initial = volume.description
+            min_vol_size = volume.size
+            size_help_text = (_('Volume size must be equal to or greater '
+                                'than the origin volume size (%sGiB)')
+                              % volume.size)
+            self.fields['size'].initial = min_vol_size
+            self.fields['size'].help_text = size_help_text
+            self.fields['volume_source'].choices = ((volume.id, volume),)
+            del self.fields['snapshot_source']
+            del self.fields['image_source']
+            del self.fields['volume_source_type']
+
 
 volume_views.CreateView.form_class = NectarVolumeCreateForm
 
