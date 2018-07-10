c3cc827b4262f813aa8f912ad70c26f2b6215c82
Sam Morrison <sorrison@gmail.com>
2018-07-10 10:12:57 +1000

    Large to allocation API to use keystone and support roles
    Change-Id: I783ea70b5c3504eb00888b430040e4e65a8c7a15

---
+++ nectar_dashboard/rcallocation/tests/base.py
@@ -0,0 +1,29 @@
+#   Licensed under the Apache License, Version 2.0 (the "License"); you may
+#   not use this file except in compliance with the License. You may obtain
+#   a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#   WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#   License for the specific language governing permissions and limitations
+#   under the License.
+#
+
+from rest_framework import test
+
+from nectar_dashboard.rcallocation.tests import factories
+from nectar_dashboard.rcallocation.tests import utils
+
+
+class AllocationAPITest(test.APITestCase):
+
+    def setUp(self, *args, **kwargs):
+        self.user = utils.get_user()
+        self.user2 = utils.get_user(id='user2')
+        self.approver_user = utils.get_user(id='approver',
+                                            roles=['tenantmanager'])
+        self.admin_user = utils.get_user(id='admin', roles=['admin'])
+        self.allocation = factories.AllocationFactory.create(
+            created_by=self.user.id, status='E', create_quotas=False)
