0ac7d64f2ac9c6991d30ad0f3acd13b4f04073a1
Sam Morrison <sorrison@gmail.com>
2018-07-26 11:33:57 +1000

    User contact email for authZ in allocation API
    Change-Id: I692936e395af0aed6d6c7f930d769a8b331335e9

--- nectar_dashboard/rcallocation/tests/utils.py
+++ nectar_dashboard/rcallocation/tests/utils.py
@@ -11,22 +11,27 @@
 #   under the License.
 #
 
+import mock
+
 from django.conf import settings
 
 from openstack_auth import user
 
 
-def get_user(id='123', username='bob', tenant_name='foo', roles=['member']):
+def get_user(id='123', username='bob', project_name='foo', roles=['member']):
     roles = [{'name': role} for role in roles]
+    project_id = 'id'+project_name
+    project = {'id': project_id}
+    token = mock.Mock(project=project, tenant=project)
     return user.User(id=id,
-                     token='fake',
+                     token=token,
                      user=username,
                      domain_id='default',
                      user_domain_name='Default',
-                     tenant_id=tenant_name,
-                     tenant_name=tenant_name,
+                     tenant_id=project_id,
+                     tenant_name=project_name,
                      service_catalog={},
                      roles=roles,
                      enabled=True,
-                     authorized_tenants=[tenant_name],
+                     authorized_tenants=[project_name],
                      endpoint=settings.OPENSTACK_KEYSTONE_URL)
