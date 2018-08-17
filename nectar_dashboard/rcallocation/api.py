7456a140da088c71a11941fafa9cfb1472dfba0b
Sam Morrison <sorrison@gmail.com>
2018-08-17 15:18:31 +1000

    Display Chief Investigator in allocation results
    Change-Id: Ida6408abd6b631823c5458c4064d8dea408105b6

--- nectar_dashboard/rcallocation/api.py
+++ nectar_dashboard/rcallocation/api.py
@@ -200,6 +200,7 @@ class QuotaViewSet(viewsets.ModelViewSet, PermissionMixin):
 class AllocationSerializer(serializers.ModelSerializer):
     quotas = QuotaGroupsField(many=False, read_only=True)
     status_display = serializers.SerializerMethodField()
+    chief_investigator = serializers.SerializerMethodField()
 
     class Meta:
         model = models.AllocationRequest
@@ -213,6 +214,15 @@ class AllocationSerializer(serializers.ModelSerializer):
     @staticmethod
     def get_status_display(obj):
         return obj.get_status_display()
+
+    @staticmethod
+    def get_chief_investigator(obj):
+        investigators = obj.investigators.all()
+        if investigators:
+            # Should only be one ci per allocation
+            ci = investigators[0]
+            return str(ci.email)
+        return None
 
     def validate_project_name(self, value):
         projects = models.AllocationRequest.objects.filter(project_name=value)
@@ -234,6 +244,7 @@ class AdminAllocationSerializer(AllocationSerializer):
 class AllocationFilter(filters.FilterSet):
     parent_request__isnull = filters.BooleanFilter(name='parent_request',
                                                    lookup_expr='isnull')
+    chief_investigator = filters.CharFilter('investigators__email')
 
     class Meta:
         model = models.AllocationRequest
@@ -246,7 +257,7 @@ class AllocationViewSet(viewsets.ModelViewSet, PermissionMixin):
     queryset = models.AllocationRequest.objects.prefetch_related(
         'quotas', 'quotas__quota_set', 'quotas__zone',
         'quotas__quota_set__resource__service_type',
-        'quotas__quota_set__resource')
+        'quotas__quota_set__resource', 'investigators')
     serializer_class = AllocationSerializer
     filter_class = AllocationFilter
 
@@ -257,7 +268,7 @@ class AllocationViewSet(viewsets.ModelViewSet, PermissionMixin):
             contact_email=self.request.user.username).prefetch_related(
                 'quotas', 'quotas__quota_set', 'quotas__zone',
                 'quotas__quota_set__resource__service_type',
-                'quotas__quota_set__resource')
+                'quotas__quota_set__resource', 'investigators')
 
     def perform_create(self, serializer):
         kwargs = {'created_by': self.request.user.token.project['id']}
