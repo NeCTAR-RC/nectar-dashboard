5b2d29f8851b6e8a35d30533ac78e2a278ea6b23
Sam Morrison <sorrison@gmail.com>
2018-09-27 16:26:37 +1000

    Support different resource types
    Can have boolean resources which don't have a quota value,
    just an on/off value. Eg. for access to private flavours.
    
    Change-Id: I68b41f27ffa373f585e9c0f2fabb731c097f7aad

---
+++ nectar_dashboard/rcallocation/migrations/0020_resource_resource_type.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('rcallocation', '0019_add-notifications-field'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='resource',
+            options={'ordering': ['id']},
+        ),
+        migrations.AddField(
+            model_name='resource',
+            name='resource_type',
+            field=models.CharField(default=b'integer', max_length=10, choices=[(b'integer', b'Integer'), (b'boolean', b'Boolean')]),
+        ),
+    ]
