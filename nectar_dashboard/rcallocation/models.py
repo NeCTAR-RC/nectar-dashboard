9f10f9af6e918785ac813c78c37e073f2ea2f524
Sam Morrison <sorrison@gmail.com>
2018-09-27 16:26:37 +1000

    Support different resource types
    Can have boolean resources which don't have a quota value,
    just an on/off value. Eg. for access to private flavours.
    
    Change-Id: I68b41f27ffa373f585e9c0f2fabb731c097f7aad

--- nectar_dashboard/rcallocation/models.py
+++ nectar_dashboard/rcallocation/models.py
@@ -538,12 +538,21 @@ class ServiceType(models.Model):
 
 
 class Resource(models.Model):
+    INTEGER = 'integer'
+    BOOLEAN = 'boolean'
+    RESOURCE_TYPES = (
+        (INTEGER, 'Integer'),
+        (BOOLEAN, 'Boolean'),
+    )
     name = models.CharField(max_length=64)
     service_type = models.ForeignKey(ServiceType)
     quota_name = models.CharField(max_length=32)
     unit = models.CharField(max_length=32)
     requestable = models.BooleanField(default=True)
     help_text = models.TextField(null=True, blank=True)
+    resource_type = models.CharField(max_length=10, blank=False,
+                                     choices=RESOURCE_TYPES,
+                                     default=INTEGER)
 
     def __unicode__(self):
         return self.name
