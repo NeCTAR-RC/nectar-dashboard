c66bf205df2704a97fba555ff387e9a991507642
Stephen Crawley <s.crawley@uq.edu.au>
2019-04-17 16:37:03 +1000

    Tidy up the rcallocations tests
    1) Share service/resource/group factory setup to make it easier to
    mention quotas for more resources.
    2) Correct fragility in the way that quotas are checked in some testcases.
    3) Replace some "assert" statements with self.assert* calls.
    
    Change-Id: I0584b930312614fc8cdfeb6be5e44d3cf7f0425c
    
    Remove stuff that belongs in the quota_sanity commits
    
    Change-Id: Ieb4e4c890536bf6dbf58da64bae7afa608323dc6
    
    asserts
    
    Change-Id: I22239c94eecde39f1817e1a73eb68a8a147e1aca

--- nectar_dashboard/rcallocation/tests/test_user_allocations_views.py
+++ nectar_dashboard/rcallocation/tests/test_user_allocations_views.py
@@ -32,7 +32,7 @@ class RequestTestCase(TestCase):
                           institutions=[], publications=[],
                           grants=[], investigators=[], **attributes):
         for field, value in attributes.items():
-            assert getattr(model, field) == value
+            self.assertEqual(getattr(model, field), value)
         self.assertEqual(model.contact_email, self.user.name)
         # Sort by requested quota so we can easily compare by iterating
         quotas_l = models.Quota.objects.filter(
@@ -49,19 +49,21 @@ class RequestTestCase(TestCase):
 
         institutions_l = model.institutions.all()
         for i, institution_model in enumerate(institutions_l):
-            assert institution_model.name == institutions[i]['name']
+            self.assertEqual(institution_model.name, institutions[i]['name'])
 
         publications_l = model.publications.all()
         for i, pub_model in enumerate(publications_l):
-            assert pub_model.publication == publications[i]['publication']
+            self.assertEqual(pub_model.publication,
+                             publications[i]['publication'])
 
         grants_l = model.grants.all()
         for i, grant_model in enumerate(grants_l):
-            assert grant_model.grant_type == grants[i]['grant_type']
+            self.assertEqual(grant_model.grant_type, grants[i]['grant_type'])
 
         investigators_l = model.investigators.all()
         for i, investigator_model in enumerate(investigators_l):
-            assert investigator_model.email == investigators[i]['email']
+            self.assertEqual(investigator_model.email,
+                             investigators[i]['email'])
 
     @mock.patch.object(api.nova, 'tenant_absolute_limits')
     def test_edit_allocation_request(self, mock_nova_limits):
@@ -112,4 +114,5 @@ class RequestTestCase(TestCase):
             del quota['id']
             del quota['allocation']
 
-        assert old_state == initial_state
+        self.assertEqual(old_state, initial_state,
+                         msg="allocation fields changed unexpectedly")
