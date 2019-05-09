6546ea82e334db3079d2054e553af3c5b4871380
Sam Morrison <sorrison@gmail.com>
2019-05-10 08:48:43 +1000

    Refactor tests for rcallocations
    Clean up a but of duplication in factories and the likes
    
    Co-authored-by: Stephen Crawley <s.crawley@uq.edu.au>
    
    Change-Id: I9a1e7cee8d05a5fcbb94dbb422c9c19d2c21003a

--- nectar_dashboard/rcallocation/tests/test_user_allocations_views.py
+++ nectar_dashboard/rcallocation/tests/test_user_allocations_views.py
@@ -14,31 +14,35 @@
 from operator import itemgetter
 
 from django.core.urlresolvers import reverse
-
 import mock
+from openstack_dashboard import api
 
 from nectar_dashboard.rcallocation import models
 
-from openstack_dashboard import api
-from openstack_dashboard.test.helpers import TestCase
+from nectar_dashboard.rcallocation.tests import base
+from nectar_dashboard.rcallocation.tests import common
 
 from .factories import AllocationFactory
-from .common import allocation_to_dict, request_allocation
 
 
-class RequestTestCase(TestCase):
+class RequestTestCase(base.BaseTestCase):
 
-    def assert_allocation(self, model, quotas=[],
+    def assert_allocation(self, model, quotas=[], requestable=True,
                           institutions=[], publications=[],
                           grants=[], investigators=[], **attributes):
         for field, value in attributes.items():
-            assert getattr(model, field) == value
+            self.assertEqual(getattr(model, field), value)
         self.assertEqual(model.contact_email, self.user.name)
         # Sort by requested quota so we can easily compare by iterating
-        quotas_l = models.Quota.objects.filter(
-            group__allocation=model).order_by('requested_quota')
+        # (Note that this is a bit dodgy since the requested quotas are
+        # typically fuzzed ... and there is a small probability that two
+        # different quota values will be the same.)
+        kwargs = {'group__allocation': model}
+        if requestable:
+            kwargs['resource__requestable'] = True
+        quotas_l = models.Quota.objects.filter(**kwargs) \
+                   .order_by('requested_quota')
         quotas = sorted(quotas, key=itemgetter('requested_quota'))
-
         self.assertEqual(quotas_l.count(), len(quotas))
         for i, quota_model in enumerate(quotas_l):
             self.assertEqual(quota_model.resource.id, quotas[i]['resource'])
@@ -49,19 +53,21 @@ class RequestTestCase(TestCase):
 
         institutions_l = model.institutions.all()
         for i, institution_model in enumerate(institutions_l):
-            assert institution_model.name == institutions[i]['name']
+            self.assertEqual(institution_model.name, institutions[i]['name'])
 
         publications_l = model.publications.all()
         for i, pub_model in enumerate(publications_l):
-            assert pub_model.publication == publications[i]['publication']
+            self.assertEqual(pub_model.publication,
+                             publications[i]['publication'])
 
         grants_l = model.grants.all()
         for i, grant_model in enumerate(grants_l):
-            assert grant_model.grant_type == grants[i]['grant_type']
+            self.assertEqual(grant_model.grant_type, grants[i]['grant_type'])
 
         investigators_l = model.investigators.all()
         for i, investigator_model in enumerate(investigators_l):
-            assert investigator_model.email == investigators[i]['email']
+            self.assertEqual(investigator_model.email,
+                             investigators[i]['email'])
 
     @mock.patch.object(api.nova, 'tenant_absolute_limits')
     def test_edit_allocation_request(self, mock_nova_limits):
@@ -69,15 +75,15 @@ class RequestTestCase(TestCase):
         mock_nova_limits.return_value = {}
 
         allocation = AllocationFactory.create(contact_email=self.user.name)
-        initial_state = allocation_to_dict(
+        initial_state = common.allocation_to_dict(
             models.AllocationRequest.objects.get(pk=allocation.pk))
 
         response = self.client.get(
             reverse('horizon:allocation:user_requests:edit_request',
                     args=(allocation.id,)))
         self.assertStatusCode(response, 200)
-        expected_model, form = request_allocation(user=self.user,
-                                                  model=allocation)
+        expected_model, form = common.request_allocation(user=self.user,
+                                                         model=allocation)
 
         response = self.client.post(
             reverse('horizon:allocation:user_requests:edit_request',
@@ -100,7 +106,7 @@ class RequestTestCase(TestCase):
         # check historical allocation model
         old_model = (models.AllocationRequest.objects.get(
             parent_request_id=model.id))
-        old_state = allocation_to_dict(old_model)
+        old_state = common.allocation_to_dict(old_model)
 
         # some fields are changed during the archive process, these
         # fields should not be compared.
@@ -112,4 +118,5 @@ class RequestTestCase(TestCase):
             del quota['id']
             del quota['allocation']
 
-        assert old_state == initial_state
+        self.assertEqual(old_state, initial_state,
+                         msg="allocation fields changed unexpectedly")
