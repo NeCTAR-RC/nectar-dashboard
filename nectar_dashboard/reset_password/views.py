NeCTAR-RC/nectar-dashboard

commit fd0af04198e027e0a5151758ad069bb7ff5c2413

Fix up all flake/pep8 warnings and now run flake8 job on all lines
Change-Id: Ia6bb10d4ca579d048f997a61d4554c5f269d3513

nectar_dashboard/reset_password/views.py
@@ -1,19 +1,17 @@
 import base64
+import hashlib
 import logging
 import random
 import time
-import sha
 
-from django.contrib import messages
 from django.conf import settings
+from django.contrib import messages
 from django import forms
 from django import shortcuts
-
-from keystoneclient.v3 import client as keystoneclient
 from keystoneauth1.identity import v3
-from openstack_dashboard import api
-from openstack_auth import utils
 from openstack_auth import user as auth_user
+from openstack_auth import utils
+from openstack_dashboard import api
 
 
 LOG = logging.getLogger(__name__)
@@ -27,7 +25,7 @@ def credentials(request):
     if request.method == 'POST':
         passwordForm = forms.Form(request.POST)
         sys_random = random.SystemRandom()
-        password = sha.sha(str(sys_random.getrandbits(256))).hexdigest()
+        password = hashlib.sha(str(sys_random.getrandbits(256))).hexdigest()
         password = base64.encodestring(password)[:20]
 
         project_id = request.user.token.tenant['id']

nectar_dashboard/project_members/views.py
@@ -1,5 +1,3 @@
-# vim: tabstop=4 shiftwidth=4 softtabstop=4
-
 # Copyright 2013 Hewlett-Packard Development Company, L.P.
 #
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
@@ -36,6 +34,7 @@ class User(object):
     def __init__(self, user_dict):
         for k, v in user_dict.items():
             setattr(self, k, v)
+
 
 class ProjectManageMixin(object):
     def _get_project(self):
@@ -78,7 +77,7 @@ class ManageMembersView(ProjectManageMixin, tables.DataTableView):
         project_members = []
         try:
             project_members = self._get_project_members()
-        except:
+        except Exception:
             exceptions.handle(self.request,
                               _('Unable to retrieve project users.'))
         return project_members

nectar_dashboard/rcallocation/allocation/views.py
@@ -1,10 +1,10 @@
 from django.views.generic.edit import UpdateView
 
-from nectar_dashboard.rcallocation import models
 from nectar_dashboard.rcallocation import forms
+from nectar_dashboard.rcallocation import mixins
+from nectar_dashboard.rcallocation import models
 from nectar_dashboard.rcallocation import utils
 from nectar_dashboard.rcallocation import views
-from nectar_dashboard.rcallocation import mixins
 
 from nectar_dashboard.rcallocation.allocation import forms as allocation_forms
 

nectar_dashboard/rcallocation/allocation_approved/views.py
@@ -3,11 +3,13 @@ import logging
 from nectar_dashboard.rcallocation.models import AllocationRequest
 from nectar_dashboard.rcallocation.views import AllocationsListView
 
+
 LOG = logging.getLogger('nectar_dashboard.rcallocation')
 
 
 class ApprovedAllocationsListView(AllocationsListView):
     page_title = 'Approved Requests'
+
     def get_data(self):
         return [ar for ar in
                 AllocationRequest.objects.filter(

nectar_dashboard/rcallocation/request/views.py
@@ -1,7 +1,5 @@
 from django.core.urlresolvers import reverse
 from django.forms.models import inlineformset_factory
-from django.http import HttpResponseRedirect
-from django.db import transaction
 
 from nectar_dashboard.rcallocation import forms
 from nectar_dashboard.rcallocation import models

nectar_dashboard/rcallocation/user_allocations/views.py
@@ -1,20 +1,14 @@
 import logging
-from collections import defaultdict
 
 from django.db.models import Q
-from django.forms.models import inlineformset_factory
-from django.core.exceptions import PermissionDenied
-
-from horizon.utils.memoized import memoized
 from openstack_dashboard.api import keystone
 
-from nectar_dashboard.rcallocation import forms
 from nectar_dashboard.rcallocation import models
+from nectar_dashboard.rcallocation.user_allocations import forms
+from nectar_dashboard.rcallocation.user_allocations import tables
 from nectar_dashboard.rcallocation import utils
 from nectar_dashboard.rcallocation import views
 
-from .forms import UserAllocationRequestForm
-from .tables import UserAllocationListTable
 
 LOG = logging.getLogger('nectar_dashboard.rcallocation')
 
@@ -31,7 +25,7 @@ class RestrictedAllocationsEditView(BaseAllocationUpdateView):
     page_title = 'Update'
     template_name = "rcallocation/allocationrequest_user_update.html"
     model = models.AllocationRequest
-    form_class = UserAllocationRequestForm
+    form_class = forms.UserAllocationRequestForm
     success_url = "../../"
 
 
@@ -63,7 +57,7 @@ def get_managed_projects(request):
             user=request.user.id,
             include_subtree=False,
             include_names=True)
-    except:
+    except Exception:
         role_assignments = []
 
     for ra in role_assignments:
@@ -74,13 +68,12 @@ def get_managed_projects(request):
 
 
 class UserAllocationsListView(views.AllocationsListView):
-    """
-    A simple paginated view of the allocation requests, ordered by
+    """A simple paginated view of the allocation requests, ordered by
     status. Later we should perhaps add sortable columns, filterable
     by status?
     """
     context_object_name = "allocation_list"
-    table_class = UserAllocationListTable
+    table_class = tables.UserAllocationListTable
     template_name = 'rcallocation/allocationrequest_user_list.html'
     page_title = 'My Requests'
 
@@ -89,8 +82,8 @@ class UserAllocationsListView(views.AllocationsListView):
         managed_projects = get_managed_projects(self.request)
         return (models.AllocationRequest.objects.all()
                 .filter(parent_request=None)
-                .filter(Q(project_id__in=managed_projects) |
-                        Q(contact_email__exact=contact_email))
+                .filter(Q(project_id__in=managed_projects)
+                        | Q(contact_email__exact=contact_email))
                 .order_by('status')
                 .prefetch_related('quotas'))
 

nectar_dashboard/rcallocation/views.py
@@ -1,28 +1,27 @@
 from collections import OrderedDict
-import logging
 import json
+import logging
 from operator import methodcaller
 import re
 
-from django.views.generic import DetailView
-from django.views.generic.edit import UpdateView
-from django.views.generic.edit import ModelFormMixin
-from django.forms.models import inlineformset_factory
-from django.db import transaction
 from django.db.models import Q
+from django.db import transaction
+from django.forms.models import inlineformset_factory
 from django.http import HttpResponseRedirect
 from django.template import loader
+from django.views.generic import DetailView
+from django.views.generic.edit import ModelFormMixin
+from django.views.generic.edit import UpdateView
 from horizon import tables as horizon_tables
-
 from novaclient import exceptions as n_exc
-
 from openstack_dashboard import api
 
-from nectar_dashboard.rcallocation import models
 from nectar_dashboard.rcallocation import forms
+from nectar_dashboard.rcallocation import mixins
+
+from nectar_dashboard.rcallocation import models
 from nectar_dashboard.rcallocation import tables
 from nectar_dashboard.rcallocation import utils
-from nectar_dashboard.rcallocation import mixins
 
 
 LOG = logging.getLogger('nectar_dashboard.rcallocation')
@@ -30,8 +29,7 @@ LOG = logging.getLogger('nectar_dashboard.rcallocation')
 
 class AllocationDetailView(mixins.UserPassesTestMixin, DetailView,
                            ModelFormMixin):
-    """
-    A class that handles rendering the details view, and then the
+    """A class that handles rendering the details view, and then the
     posting of the associated accept/reject action
     """
 
@@ -59,8 +57,7 @@ class AllocationDetailView(mixins.UserPassesTestMixin, DetailView,
 
 class AllocationsListView(mixins.UserPassesTestMixin,
                           horizon_tables.DataTableView):
-    """
-    A simple paginated view of the allocation requests, ordered by
+    """A simple paginated view of the allocation requests, ordered by
     status. Later we should perhaps add sortable columns, filterable
     by status?
     """
@@ -88,8 +85,7 @@ class AllocationsListView(mixins.UserPassesTestMixin,
 
 class AllocationHistoryView(mixins.UserPassesTestMixin,
                             horizon_tables.DataTableView):
-    """
-    A simple paginated view of the allocation requests, ordered by
+    """A simple paginated view of the allocation requests, ordered by
     status. Later we should perhaps add sortable columns, filterable
     by status?
     """
@@ -563,8 +559,7 @@ class BaseAllocationView(mixins.UserPassesTestMixin, UpdateView):
     def form_invalid(self, form, investigator_formset=None,
                      institution_formset=None, publication_formset=None,
                      grant_formset=None, quota_formsets=None):
-        """
-        If the form is invalid, re-render the context data with the
+        """If the form is invalid, re-render the context data with the
         data-filled forms and errors.
         """
         return self.render_to_response(

nectar_dashboard/project_members/forms.py
@@ -19,14 +19,12 @@ import logging
 from django.conf import settings
 from django.utils.safestring import mark_safe
 from django.utils.translation import ugettext_lazy as _
-
 from horizon import exceptions
 from horizon import forms
 from horizon import messages
-
 from openstack_dashboard import api
 
-from . import api as api_ext
+from nectar_dashboard.project_members import api as api_ext
 
 
 LOG = logging.getLogger(__name__)
@@ -48,7 +46,7 @@ class AddUserToProjectForm(forms.SelfHandlingForm):
                                               role=role_id)
             messages.success(request,
                              _('User added successfully.'))
-        except:
+        except Exception:
             exceptions.handle(request, _('Unable to add user to project.'))
             return False
         return True

commit 17eb19c575e09762597f7ec03c9bc85d7761b535

Reset-password: Fix up import of sha
Bug was introduced in fd0af04198e027e0a5151758ad069bb7ff5c2413

Change-Id: Ifb77558c86ed615d3368826602727e1d06f0ceee

nectar_dashboard/reset_password/views.py
@@ -25,7 +25,7 @@ def credentials(request):
     if request.method == 'POST':
         passwordForm = forms.Form(request.POST)
         sys_random = random.SystemRandom()
-        password = hashlib.sha(str(sys_random.getrandbits(256))).hexdigest()
+        password = hashlib.sha1(str(sys_random.getrandbits(256))).hexdigest()
         password = base64.encodestring(password)[:20]
 
         project_id = request.user.token.tenant['id']