5089bac38aef3e4c2e5c8848e3e8f324e49b5726
Andy Botting <andy@andybotting.com>
2019-06-11 12:20:07 +1000

    Updates to dashboard panels
    This commit does the following:
     * Re-enabled the identity panel (for App Creds)
     * Moves 'Users' panel to 'Identity/Project Members'
     * Disables Projects/Users/Groups/Roles under Identity
    
    Change-Id: I9bc12fbd881bedd2eecf538523c9f58434035fbc

--- enabled/_9010_identity_project_members_panel.py
+++ enabled/_9010_identity_project_members_panel.py
@@ -1,9 +1,12 @@
 # The slug of the panel to be added to HORIZON_CONFIG. Required.
 PANEL = 'members'
 # The slug of the dashboard the PANEL associated with. Required.
-PANEL_DASHBOARD = 'project'
+PANEL_DASHBOARD = 'identity'
 # The slug of the panel group the PANEL is associated with.
-PANEL_GROUP = 'project'
+PANEL_GROUP = 'default'
+
+# If set, it will update the default panel of the PANEL_DASHBOARD.
+DEFAULT_PANEL = 'members'
 
 # Python panel class of the PANEL to be added.
 ADD_PANEL = \
