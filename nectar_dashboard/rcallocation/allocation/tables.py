5161fcb691215b2a2388e60acabf78ba2505611d
Sam Morrison <sorrison@gmail.com>
2019-07-04 16:01:21 +1000

    Show submitted date as opposed to expiry in pending list
    Change-Id: Ide4f39665f5321c76871997f67caf2a91d52c0dd

--- nectar_dashboard/rcallocation/allocation/tables.py
+++ nectar_dashboard/rcallocation/allocation/tables.py
@@ -6,10 +6,18 @@ from nectar_dashboard.rcallocation.tables import ViewHistory
 
 
 class PendingAllocationListTable(BaseAllocationListTable):
+
+    approver = tables.Column('approver_email',
+                             verbose_name='Previous Approver')
+    submit_date = tables.Column('submit_date',
+                                verbose_name='Submitted Date')
+
+    def __init__(self, *args, **kwargs):
+        super(PendingAllocationListTable, self).__init__(*args, **kwargs)
+        self.columns.pop('end_date')
+
     class Meta:
         verbose_name = "Pending Requests"
         table_actions = (tables.NameFilterAction,)
         row_actions = (EditRequest, ViewHistory,)
 
-    approver = tables.Column('approver_email',
-                             verbose_name='Previous Approver')
