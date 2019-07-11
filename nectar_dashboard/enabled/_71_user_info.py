a8cc1f05204a0eb10501e3f1cfc0e0f4381d13c5
Stephen Crawley <s.crawley@uq.edu.au>
2019-07-11 13:52:49 +1000

    Adds the user_info horizon plugin
    This plugin adds Settings > User Info Update and Identity > User
    Info Lookup panesl to the dashboards.  The first one allows a user
    to see and update their RCShibboleth user information.  The second
    allows an user (admin) to lookup and view information about other
    users.
    
    The current version uses some icky stuff to hook into the RCShibboleth
    database table.  This requires a database touter config in the local
    settings file.
    
    The panels are configured by:
       _71_user_info.py  (lookup)
       _72_my_user_info_panel.py (update)
    
    The former depends on the latter.
    
    TODO: short term:  styling fixes, "saved" notification, unit tests
          medium term: should admins be able to update user info?
                       do we need a new permission?
          long term:   redo the RCShibboleth integration to uas an API
    Change-Id: I540a7ec5edd26eb15d1bdeccd29d5d009978a599

---
+++ nectar_dashboard/enabled/_71_user_info.py
@@ -0,0 +1,27 @@
+# The name of the dashboard to be added to HORIZON['dashboards']. Required.
+PANEL_DASHBOARD = 'identity'
+
+PANEL_GROUP = 'default'
+
+PANEL = 'lookup'
+
+# If set to True, this dashboard will not be added to the settings.
+DISABLED = False
+
+
+# Python panel class of the PANEL to be added.
+ADD_PANEL = \
+    'nectar_dashboard.user_info.lookup.panel.UserLookupPanel'
+
+# A list of applications to be added to INSTALLED_APPS.
+ADD_INSTALLED_APPS = [
+    'nectar_dashboard.user_info',
+    'nectar_dashboard.user_info.lookup',
+]
+
+AUTO_DISCOVER_STATIC_FILES = False
+
+ADD_SCSS_FILES = [
+    'rcportal/scss/nectar.scss',
+    'rcportal/scss/bootstrap-toggle.scss',
+]
