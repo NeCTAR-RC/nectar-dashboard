39712d3b18ddd805049e8bc1bc2e01b5f42ad89f
Stephen Crawley <s.crawley@uq.edu.au>
2019-07-11 13:52:49 +1000

    First checkin of the user_info app.  Work in progress.
    Change-Id: Ie9fe11e19d84fe24c6a2e414a2af2e1ac90f1f7e

---
+++ nectar_dashboard/user_info/lookup/views.py
@@ -0,0 +1,54 @@
+# Copyright 2019 Australian Research Data Commons
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from django.core.urlresolvers import reverse
+from django.db.models import Q
+from django.views.generic import edit
+
+from horizon import tables as horizon_tables
+
+from nectar_dashboard.user_info import models
+
+from . import forms
+from . import tables
+
+
+class UserLookupView(edit.FormView):
+    """A simple form view for user lookup
+    """
+
+    form_class = forms.UserLookupForm
+    template_name = "user_info/lookup.html"
+
+    def form_valid(self, form):
+        self.form = form
+        return super(UserLookupView, self).form_valid(form)
+
+    def get_success_url(self):
+        email = self.form.cleaned_data['email']
+        return reverse('horizon:user-info:lookup:list',
+                       kwargs={'email': email})
+
+
+class UserListView(horizon_tables.DataTableView):
+    """A simple listing of users matching a lookup
+    """
+
+    table_class = tables.UsersTable
+    template_name = "user_info/list.html"
+
+    def get_data(self):
+        email = self.kwargs['email']
+        return models.User.objects.filter(Q(user_id__iexact=email)
+                                          | Q(email__iexact=email))
