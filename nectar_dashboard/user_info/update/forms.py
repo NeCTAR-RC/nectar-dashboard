940df95298183b6928c633ee36301a6ee7094e8a
Stephen Crawley <s.crawley@uq.edu.au>
2019-07-11 13:52:49 +1000

    Adds the user_info horizon plugin
    This plugin adds Settings > User Info Update and Identity > User
    Info Lookup panesl to the dashboards.  The first one allows a user
    to see and update their RCShibboleth user information.  The second
    allows an user (admin) to lookup and view information about other
    users.
    
    The current version uses some icky stuff to hook into the RCShibboleth
    database table.  This requires a database router config in the local
    settings file.
    
    The panels are configured by:
       _71_user_info.py  (lookup)
       _72_my_user_info_panel.py (update)
    
    The former depends on the latter.
    
    TODO: short term:  styling fixes, "saved" notification
                       orcid lookup + "assist"
          medium term: should admins be able to update user info?
                       do we need a new permission?
          long term:   redo the RCShibboleth integration to use an API
    
    Change-Id: I540a7ec5edd26eb15d1bdeccd29d5d009978a599

---
+++ nectar_dashboard/user_info/update/forms.py
@@ -0,0 +1,37 @@
+# Copyright 2019 Australian Research Data Commons
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import logging
+
+from nectar_dashboard.user_info import forms as base_forms
+
+LOG = logging.getLogger(__name__)
+
+
+class UserEditForm(base_forms.UserBaseForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['affiliation'].required = True
+
+    def clean(self):
+        super().clean()
+        # In the functional tests, something seems to be turning
+        # None (DB NULL) values into 'None'.
+        if self.cleaned_data['orcid'] == 'None':
+            self.cleaned_data['orcid'] = None
+        if self.cleaned_data['phone_number'] == 'None':
+            self.cleaned_data['phone_number'] = None
+        if self.cleaned_data['mobile_number'] == 'None':
+            self.cleaned_data['mobile_number'] = None
