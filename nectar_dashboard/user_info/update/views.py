4a97e910e02fba4d96b5108c738522cfdf1a98a4
Stephen Crawley <s.crawley@uq.edu.au>
2019-07-11 13:52:49 +1000

    Adds the user_info horizon plugin
    This plugin adds Settings > User Info Update and Identity > User
    Info Lookup panesl to the dashboards.  The first one allows a user
    to see and update their RCShibboleth user information.  The second
    allows an user (admin) to lookup and view information about other
    users.
    
    The current version uses some icky stuff to hook into the RCShibboleth
    database table.  This requires a database touter config in the local
    settings file.
    
    The panels are configured by:
       _71_user_info.py  (lookup)
       _72_my_user_info_panel.py (update)
    
    The former depends on the latter.
    
    TODO: short term: access control, final styling, unit tests
          long term: redo the RCShibboleth integration to uas an API
    Change-Id: I540a7ec5edd26eb15d1bdeccd29d5d009978a599

---
+++ nectar_dashboard/user_info/update/views.py
@@ -0,0 +1,36 @@
+# Copyright 2019 Australian Research Data Commons
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from django.views.generic import edit
+
+from nectar_dashboard.user_info import models
+from nectar_dashboard.user_info import views
+
+from . import forms
+
+
+class UserEditView(views.PageTitleMixin, edit.UpdateView):
+    """A simple form view for editing the user's details
+    """
+    model = models.User
+    form_class = forms.UserEditForm
+    template_name = "user_info/edit.html"
+    page_title = "User Info Update"
+
+
+class UserEditSelfView(UserEditView):
+
+    def get_object(self):
+        user_id = self.request.user.keystone_user_id
+        return models.User.objects.get(user_id=user_id)
