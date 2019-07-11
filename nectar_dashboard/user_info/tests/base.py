b92a1c38153b0edf4073e1db8aff472683b224c4
Stephen Crawley <s.crawley@uq.edu.au>
2019-07-11 13:52:49 +1000

    Adds the user_info horizon plugin
    This plugin adds Settings > User Info Update and Identity > User
    Info Lookup panesl to the dashboards.  The first one allows a user
    to see and update their RCShibboleth user information.  The second
    allows an user (admin) to lookup and view information about other
    users.
    
    The current version uses some icky stuff to hook into the RCShibboleth
    database table.  This requires a database touter config in the local
    settings file.
    
    The panels are configured by:
       _71_user_info.py  (lookup)
       _72_my_user_info_panel.py (update)
    
    The former depends on the latter.
    
    TODO: short term:  styling fixes, "saved" notification
          medium term: should admins be able to update user info?
                       do we need a new permission?
    	           validation / assistance for orcids
          long term:   redo the RCShibboleth integration to use an API
    Change-Id: I540a7ec5edd26eb15d1bdeccd29d5d009978a599

---
+++ nectar_dashboard/user_info/tests/base.py
@@ -0,0 +1,69 @@
+# Copyright 2019 Australian Research Data Commons
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from openstack_dashboard.test import helpers
+
+from keystoneclient.v3 import roles
+
+from . import common
+
+
+class BaseTestCase(helpers.BaseAdminViewTests):
+
+    def setUp(self):
+        super().setUp()
+        common.factory_setup()
+        self.rcs_user = common.UserFactory(email="joe.smith@gmail.com")
+
+    def assertEqualUsers(self, user1, user2, ignore=[]):
+        """Assert two RCS users are the same.  The users can be represented
+        as User model objects or as dicts.  The 'ignore' argument is a
+        list of keys / attributes to ignore when comparing.
+        """
+        dict1 = user1 if type(user1) is dict else common.user_to_dict(user1)
+        dict2 = user2 if type(user2) is dict else common.user_to_dict(user2)
+        for k in ignore:
+            dict1.pop(k, None)
+            dict2.pop(k, None)
+        self.assertEqual(dict1, dict2)
+
+
+class UserViewTestCase(BaseTestCase):
+
+    def setUp(self):
+        super().setUp()
+        self.setActiveUser(id=self.rcs_user.user_id,
+                           username=self.rcs_user.persistent_id,
+                           token=self.token,
+                           service_catalog=self.service_catalog,
+                           tenant_id=self.tenant.id)
+
+
+class AdminViewTestCase(BaseTestCase):
+
+    def setUp(self):
+        super().setUp()
+        allocation_admin_role_dict = {'id': '142',
+                                      'name': 'allocationadmin'}
+        allocation_admin_role = roles.Role(roles.RoleManager,
+                                           allocation_admin_role_dict,
+                                           loaded=True)
+        self.roles.add(allocation_admin_role)
+        self.roles.allocation_admin = allocation_admin_role
+        self.setActiveUser(id=self.rcs_user.user_id,
+                           username=self.rcs_user.persistent_id,
+                           token=self.token,
+                           service_catalog=self.service_catalog,
+                           tenant_id=self.tenant.id,
+                           roles=[self.roles.allocation_admin._info])
---
+++ nectar_dashboard/user_info/tests/test_lookup_request_views.py
@@ -0,0 +1,71 @@
+# Copyright 2019 Australian Research Data Commons
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import pdb
+
+from django.urls import reverse
+
+from . import base
+
+
+class LookupViewTestCase(base.AdminViewTestCase):
+    url = reverse('horizon:identity:lookup:lookup')
+
+    def test_get(self):
+        response = self.client.get(self.url)
+        self.assertStatusCode(response, 200)
+
+    def test_post_no_data(self):
+        response = self.client.post(self.url)
+        self.assertStatusCode(response, 200)
+        form = response.context_data['form']
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors,
+                         {'email': ['This field is required.']})
+
+    def test_post_bad_email(self):
+        form = {'email': "nobody"}
+        response = self.client.post(self.url, form)
+        self.assertStatusCode(response, 200)
+        form = response.context_data['form']
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors,
+                         {'email': ['Enter a valid email address.']})
+
+    def test_post_unknown_user(self):
+        form = {'email': "nobody@example.com"}
+        response = self.client.post(self.url, form)
+        self.assertStatusCode(response, 200)
+        form = response.context_data['form']
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors,
+                         {'email': ['No users match this account / email.']})
+        if False:
+            pdb.set_trace()
+
+
+class UserLookupViewTestCase(base.UserViewTestCase):
+    url = reverse('horizon:identity:lookup:lookup')
+
+    def test_get(self):
+        response = self.client.get(self.url)
+        self.assertStatusCode(response, 302)
+        self.assertEqual(response.get('location'),
+                         "/auth/login/?next=" + self.url)
+
+    def test_POST(self):
+        response = self.client.post(self.url)
+        self.assertStatusCode(response, 302)
+        self.assertEqual(response.get('location'),
+                         "/auth/login/?next=" + self.url)
