f70030a830ba0d34cebc17986d1bcee565798902
Stephen Crawley <s.crawley@uq.edu.au>
2019-07-11 13:52:49 +1000

    Adds the user_info horizon plugin
    This plugin adds Settings > User Info Update and Identity > User
    Info Lookup panesl to the dashboards.  The first one allows a user
    to see and update their RCShibboleth user information.  The second
    allows an user (admin) to lookup and view information about other
    users.
    
    The current version uses some icky stuff to hook into the RCShibboleth
    database table.  This requires a database touter config in the local
    settings file.
    
    The panels are configured by:
       _71_user_info.py  (lookup)
       _72_my_user_info_panel.py (update)
    
    The former depends on the latter.
    
    TODO: short term:  styling fixes, "saved" notification
          medium term: should admins be able to update user info?
                       do we need a new permission?
          long term:   redo the RCShibboleth integration to uas an API
    Change-Id: I540a7ec5edd26eb15d1bdeccd29d5d009978a599

---
+++ nectar_dashboard/user_info/tests/test_lookup_request_views.py
@@ -0,0 +1,46 @@
+# Copyright 2019 Australian Research Data Commons
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from django.urls import reverse
+
+from keystoneclient.v3 import roles
+
+# from nectar_dashboard.user_info import models
+
+from . import base
+# from . import common
+
+
+class UserLookupViewTestCase(base.BaseTestCase):
+    url = reverse('horizon:identity:lookup:lookup')
+
+    def setUp(self):
+        super().setUp()
+        allocation_admin_role_dict = {'id': '142',
+                                      'name': 'allocationadmin'}
+        allocation_admin_role = roles.Role(roles.RoleManager,
+                                           allocation_admin_role_dict,
+                                           loaded=True)
+        self.roles.add(allocation_admin_role)
+        self.roles.allocation_admin = allocation_admin_role
+        self.setActiveUser(id=self.rcs_user.user_id,
+                           username=self.rcs_user.persistent_id,
+                           token=self.token,
+                           service_catalog=self.service_catalog,
+                           tenant_id=self.tenant.id,
+                           roles=[self.roles.allocation_admin._info])
+
+    def test_get(self):
+        response = self.client.get(self.url)
+        self.assertStatusCode(response, 200)
---
+++ nectar_dashboard/user_info/update/forms.py
@@ -0,0 +1,58 @@
+# Copyright 2019 Australian Research Data Commons
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import logging
+
+from django import forms
+
+from nectar_dashboard.user_info import forms as base_forms
+
+LOG = logging.getLogger(__name__)
+
+
+class UserEditForm(base_forms.UserBaseForm):
+
+    class Meta(base_forms.UserBaseForm.Meta):
+
+        exclude = ['state', 'terms_accepted_at', 'registered_at',
+                   'terms_version', 'shibboleth_attributes',
+                   'ignore_username_not_email']
+        widgets = {
+            'persistent_id': forms.TextInput(attrs={'readonly': 'readonly'}),
+            'user_id': forms.TextInput(attrs={'readonly': 'readonly'}),
+            'displayname': forms.TextInput(attrs={'readonly': 'readonly'}),
+            'email': forms.TextInput(attrs={'readonly': 'readonly'}),
+            'first_name': forms.TextInput(attrs={'readonly': 'readonly'}),
+            'surname': forms.TextInput(attrs={'readonly': 'readonly'}),
+            'home_organization': forms.TextInput(
+                attrs={'readonly': 'readonly'}),
+            'orcid': forms.TextInput(),
+            'phone_number': forms.TextInput(),
+            'mobile_number': forms.TextInput(),
+            'affiliation': forms.Select(),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def clean(self):
+        super().clean()
+        # In the functional tests, something seems to be turning
+        # None (DB NULL) values into 'None'.
+        if self.cleaned_data['orcid'] == 'None':
+            self.cleaned_data['orcid'] = None
+        if self.cleaned_data['phone_number'] == 'None':
+            self.cleaned_data['phone_number'] = None
+        if self.cleaned_data['mobile_number'] == 'None':
+            self.cleaned_data['mobile_number'] = None
