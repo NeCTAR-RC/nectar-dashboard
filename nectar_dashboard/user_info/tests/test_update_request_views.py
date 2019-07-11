ccd53c2be46d1442442d6a43c3051d65b676daa0
Stephen Crawley <s.crawley@uq.edu.au>
2019-07-11 13:52:49 +1000

    Adds the user_info horizon plugin
    This plugin adds Settings > User Info Update and Identity > User
    Info Lookup panesl to the dashboards.  The first one allows a user
    to see and update their RCShibboleth user information.  The second
    allows an user (admin) to lookup and view information about other
    users.
    
    The current version uses some icky stuff to hook into the RCShibboleth
    database table.  This requires a database router config in the local
    settings file.
    
    The panels are configured by:
       _71_user_info.py  (lookup)
       _72_my_user_info_panel.py (update)
    
    The former depends on the latter.
    
    TODO: short term:  styling fixes, "saved" notification
                       orcid lookup + "assist"
          medium term: should admins be able to update user info?
                       do we need a new permission?
          long term:   redo the RCShibboleth integration to use an API
    
    Change-Id: I540a7ec5edd26eb15d1bdeccd29d5d009978a599

---
+++ nectar_dashboard/user_info/tests/test_update_request_views.py
@@ -0,0 +1,60 @@
+# Copyright 2019 Australian Research Data Commons
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from django.urls import reverse
+
+from nectar_dashboard.user_info import models
+
+from . import base
+from . import common
+
+
+class EditSelfViewTestCase(base.UserViewTestCase):
+    url = reverse('horizon:settings:update:edit-self')
+
+    def test_get(self):
+        response = self.client.get(self.url)
+        self.assertStatusCode(response, 200)
+        self.assertEqualUsers(self.rcs_user, response.context_data['object'])
+
+    def test_post_no_change(self):
+        form = common.user_to_dict(self.rcs_user)
+        response = self.client.post(self.url, form)
+        self.assertStatusCode(response, 302)
+        self.assertEqual(response.get('location'), self.url)
+        user = models.RCUser.objects.get(user_id=self.rcs_user.user_id)
+        self.assertEqualUsers(self.rcs_user, user)
+
+    def test_post_name_change(self):
+        form = common.user_to_dict(self.rcs_user)
+        form['displayname'] = "Jim Spriggs"
+        form['first_name'] = "Jim"
+        form['surname'] = "Spriggs"
+        response = self.client.post(self.url, form)
+        self.assertStatusCode(response, 302)
+        self.assertEqual(response.get('location'), self.url)
+        # Change should be ignored
+        user = models.RCUser.objects.get(user_id=self.rcs_user.user_id)
+        self.assertEqualUsers(user, self.rcs_user)
+
+    def test_post_orcid_change(self):
+        form = common.user_to_dict(self.rcs_user)
+        form['orcid'] = "rose"
+        response = self.client.post(self.url, form)
+        self.assertStatusCode(response, 302)
+        self.assertEqual(response.get('location'), self.url)
+
+        # Change should be made
+        user = models.RCUser.objects.get(user_id=self.rcs_user.user_id)
+        self.assertEqualUsers(user, form)
