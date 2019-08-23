36e56451b161ad22590f40008c2f0baa9dc61bc4
Stephen Crawley <s.crawley@uq.edu.au>
2019-08-24 01:30:29 +1000

    Bug fix: copy_allocation must keep modified_time
    This corrects a problem where modified timestamps in the
    history got messed up by edit and approval actions.
    
    Includes a unit + functional test
    
    Change-Id: I56824c54af356e3ef14abc8cd3b76fda0eff6424

---
+++ nectar_dashboard/rcallocation/tests/unit/utils/test_copy_allocation.py
@@ -0,0 +1,50 @@
+from openstack_dashboard.test import helpers
+
+from nectar_dashboard.rcallocation import models
+from nectar_dashboard.rcallocation.tests import common
+from nectar_dashboard.rcallocation.tests import factories
+from nectar_dashboard.rcallocation import utils
+
+
+class CopyAllocationTest(helpers.TestCase):
+
+    def setUp(self):
+        super(CopyAllocationTest, self).setUp()
+        common.factory_setup()
+
+    def test_build_test_allocation(self):
+        allocation = factories.AllocationFactory.create(
+            contact_email='other@example.com')
+        self.assertIsNone(allocation.parent_request)
+        original_dict = common.allocation_to_dict(allocation)
+        original_mod = allocation.modified_time
+
+        copy_id = utils.copy_allocation(allocation).id
+
+        # Refetch the copy from the database
+        copy = models.AllocationRequest.objects.get(id=copy_id)
+
+        # The copy's parent should refer to the allocation
+        self.assertIsNone(allocation.parent_request)
+        self.assertEqual(allocation.id, copy.parent_request.id)
+
+        copy_mod = copy.modified_time
+        current_mod = allocation.modified_time
+
+        # The copy should have the same mod date as the original
+        # allocation
+        self.assertEqual(original_mod, copy_mod)
+        self.assertEqual(original_mod, current_mod)
+
+        # Check for any other differences after removing dict entries
+        # that we've already dealt with or that we expect to be different.
+        current_dict = common.allocation_to_dict(allocation)
+        copy_dict = common.allocation_to_dict(copy)
+        for d in [original_dict, current_dict, copy_dict]:
+            d.pop('id', None)
+            d.pop('parent_request', None)
+            # ignore the quota tree for now
+            d.pop('quota', None)
+        self.maxDiff = None
+        self.assertDictEqual(copy_dict, current_dict)
+        self.assertDictEqual(copy_dict, original_dict)
--- nectar_dashboard/rcallocation/utils.py
+++ nectar_dashboard/rcallocation/utils.py
@@ -6,16 +6,31 @@ def user_is_allocation_admin(user):
 
 
 def copy_allocation(allocation):
-    old_object = models.AllocationRequest.objects.get(id=allocation.id)
+    """Create and save a history copy of 'allocation', and its component
+    objects.  The history object will have a different id, and its
+    'parent_request' will point to original allocation.  All other
+    fields (including the 'modified_time') will be the same, and
+    the copied component object graphs will be isomorphic with the
+    those of the original.
+
+    The result is the history copy for the allocation record.
+    """
+
+    manager = models.AllocationRequest.objects
+    old_object = manager.get(id=allocation.id)
     old_object.parent_request = allocation
     quota_groups = old_object.quotas.all()
     investigators = old_object.investigators.all()
     institutions = old_object.institutions.all()
     publications = old_object.publications.all()
     grants = old_object.grants.all()
+    modified_time = old_object.modified_time
 
     old_object.id = None
     old_object.save()
+    # Preserve the original modification timestamp on the old object.
+    # (This reverses the "damage" of the 'auto_now' attribute.)
+    manager.filter(id=old_object.id).update(modified_time=modified_time)
 
     for quota_group in quota_groups:
         old_quota_group_id = quota_group.id
@@ -47,3 +62,5 @@ def copy_allocation(allocation):
         grant.id = None
         grant.allocation = old_object
         grant.save()
+
+    return old_object
