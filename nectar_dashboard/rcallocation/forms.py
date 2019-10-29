b61aaa4fb3d24d8ddd6a6babef55ec202f97a7e7
Stephen Crawley <s.crawley@uq.edu.au>
2019-10-29 11:23:58 +1100

    Homes part 2. Add new fields to forms, views, api
    Change-Id: I8dd9713639cac1f04a608dff5118479e8ba95648

--- nectar_dashboard/rcallocation/forms.py
+++ nectar_dashboard/rcallocation/forms.py
@@ -20,7 +20,7 @@ class BaseAllocationForm(forms.ModelForm):
         model = models.AllocationRequest
         exclude = ('status', 'created_by', 'submit_date', 'approver_email',
                    'start_date', 'end_date', 'modified_time', 'parent_request',
-                   'allocation_home', 'provisioned',
+                   'allocation_home', 'associated_site', 'provisioned',
                    'project_id', 'notes', 'notifications')
 
         widgets = {
@@ -45,6 +45,9 @@ class BaseAllocationForm(forms.ModelForm):
                        'style': 'height:120px; width:420px'}),
             'requested_allocation_home': forms.Select(
                 attrs={'class': 'col-md-6'}),
+            'associated_site': forms.CheckboxInput(
+                attrs={'class': 'col-md-6'}),
+            'national': forms.CheckboxInput(attrs={'class': 'col-md-6'}),
             'geographic_requirements': forms.Textarea(
                 attrs={'class': 'col-md-6',
                        'style': 'height:120px; width:420px'}),
