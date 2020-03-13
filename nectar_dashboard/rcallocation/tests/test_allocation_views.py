d745f973c915554c4b7e327f257da7c715949ca7
Stephen Crawley <s.crawley@uq.edu.au>
2020-03-13 17:52:21 +1100

    New test for approval form + bug evidence
    The test shows that if the approval form is double-submitted you
    will get an extra history record with status='A'.  Fix in next commit
    
    Change-Id: Ia1bed6095f273b2e7a7887e5d8c878e449a9a620

--- nectar_dashboard/rcallocation/tests/test_allocation_views.py
+++ nectar_dashboard/rcallocation/tests/test_allocation_views.py
@@ -40,3 +40,60 @@ class ApproverRequestTestCase(base.BaseApproverTestCase):
         model_state.pop('notes')
         self.assertEqual(initial_state, model_state,
                          msg="allocation fields changed unexpectedly")
+
+    def test_approve_request(self):
+        # Prep a record in 'E' state
+        model, form = common.request_allocation(user=self.user)
+        form['ignore_warnings'] = True
+        response = self.client.post(
+            reverse('horizon:allocation:request:request'),
+            form)
+        self.assertStatusCode(response, 302)
+        self.assertTrue(response.get('location').endswith(
+            reverse('horizon:allocation:user_requests:index')),
+            msg="incorrect redirect location")
+        allocation = models.AllocationRequest.objects.get(
+            project_description=form['project_description'],
+            parent_request_id=None)
+        self.assertEqual('E', allocation.status)
+
+        print(models.AllocationRequest.objects.filter(
+            project_description=form['project_description']).count())
+
+        # Check we can get the approve form
+        url = reverse('horizon:allocation:requests:approve_request',
+                      args=(allocation.id,))
+        response = self.client.get(url)
+        self.assertStatusCode(response, 200)
+
+        # Rebuild the form ... because the formset ids need to change
+        model, form = common.request_allocation(user=self.user,
+                                                model=allocation)
+
+        # Submit the approve form
+        form['associated_site'] = common.get_site('uom').id
+        form['is_national'] = True
+        form['ignore_warnings'] = True
+        response = self.client.post(url, form)
+        self.assertStatusCode(response, 302)
+        self.assertEqual("../../", response.get('location'))
+
+        allocation = models.AllocationRequest.objects.get(id=allocation.id)
+        self.assertEqual('A', allocation.status)
+
+        print(models.AllocationRequest.objects.filter(
+            parent_request_id=allocation.id).count())
+
+        # Submit the approval again.  This needs to fail!
+        response = self.client.post(url, form)
+        self.assertStatusCode(response, 302)
+        self.assertEqual("../../", response.get('location'))
+
+        allocation = models.AllocationRequest.objects.get(id=allocation.id)
+        self.assertEqual('A', allocation.status)
+
+        print(models.AllocationRequest.objects.filter(
+            parent_request_id=allocation.id).count())
+
+        # In fact, it is currently succeeding, and creating a history
+        # record as well.  Bingo!
