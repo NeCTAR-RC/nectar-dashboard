bf26d0823841a9da73acb1fc251bd370610f841d
Sam Morrison <sorrison@gmail.com>
2020-09-16 10:18:31 +1000

    Add new field to specify whether an allocation is managed or not
    If an allocation is managed it indicates that the allocaiton cannot
    be modified in the dashboard and is managed externally.
    
    Change-Id: I4e3f459ce44f145fb3f4e269f4d3926621f11a4f

---
+++ nectar_dashboard/rcallocation/migrations/0043_add-managed-field.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('rcallocation', '0042_support_and_grant_tweaks'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='allocationrequest',
+            name='managed',
+            field=models.BooleanField(default=True, help_text=b'Whether the allocation is managed through the dashboard'),
+        ),
+    ]
