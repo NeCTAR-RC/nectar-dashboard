328f42172c21eaed271d07271acebdd64f32dedb
Sam Morrison <sorrison@gmail.com>
2020-12-02 15:00:13 +1100

    Modify trove quota sanity checks with RAM quota
    Change-Id: I6f8bd91db0a9621c0550f2f45807c718e007a1a7

--- nectar_dashboard/rcallocation/quota_sanity.py
+++ nectar_dashboard/rcallocation/quota_sanity.py
@@ -18,7 +18,7 @@ SMALL_MEM = 'SMALL_MEM'
 CINDER_WITHOUT_INSTANCES = 'CINDER_WITHOUT_INSTANCES'
 CINDER_NOT_LOCAL = 'CINDER_NOT_LOCAL'
 TROVE_WITHOUT_STORAGE = 'TROVE_WITHOUT_STORAGE'
-TROVE_WITHOUT_SERVERS = 'TROVE_WITHOUT_SERVERS'
+TROVE_WITHOUT_RAM = 'TROVE_WITHOUT_RAM'
 TROVE_WITHOUT_SWIFT = 'TROVE_WITHOUT_SWIFT'
 MANILA_NOT_LOCAL = 'MANILA_NOT_LOCAL'
 NO_ROUTER = 'NO_ROUTER'
@@ -103,23 +103,32 @@ def cinder_local_check(context):
 
 
 def trove_storage_check(context):
-    if context.get('database.instances') > 0 \
+    if context.get('database.ram') > 0 \
        and context.get('database.volumes') == 0:
         return (TROVE_WITHOUT_STORAGE,
-                'database servers requested without any database storage')
+                'Database RAM requested without any database storage')
     return None
 
 
-def trove_server_check(context):
-    if context.get('database.instances') == 0 \
+def trove_ram_check(context):
+    ram = context.get('database.ram')
+    if ram == 0 \
        and context.get('database.volumes') > 0:
-        return (TROVE_WITHOUT_SERVERS,
-                'database storage requested without any database servers')
+        return (TROVE_WITHOUT_RAM,
+                'Database storage requested without any database RAM')
+    elif ram > 0:
+        if ram < 4:
+            return ('', "Database RAM should be at least 4GB")
+        elif ram > 100:
+            return ('', "Database RAM  max limit is 100GB")
+        elif ram % 4 != 0:
+            return ('',
+                    "Database RAM should be a multiple of 4")
     return None
 
 
 def trove_backup_check(context):
-    if (context.get('database.instances') > 0
+    if (context.get('database.ram') > 0
         or context.get('database.volumes') > 0) \
        and context.get('object.object') == 0:
         return (TROVE_WITHOUT_SWIFT,
@@ -228,7 +237,7 @@ STD_CHECKS = [instance_vcpu_check,
               cinder_instance_check,
               cinder_local_check,
               trove_storage_check,
-              trove_server_check,
+              trove_ram_check,
               trove_backup_check,
               manila_local_check,
               neutron_checks,
