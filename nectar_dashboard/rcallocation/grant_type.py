d48be503d92be2adbac206f334cde3e2ce22bb27
Stephen Crawley <s.crawley@uq.edu.au>
2020-12-02 13:42:53 +1100

    (WIP) Remove ncris / ardc grant types
    Change-Id: If0bcb0f8152b389716b5078752771714eb8d182f

--- nectar_dashboard/rcallocation/grant_type.py
+++ nectar_dashboard/rcallocation/grant_type.py
@@ -2,8 +2,6 @@ GRANT_TYPES = (
     ("arc", "Australian Research Council"),
     ("nhmrc", "NHMRC"),
     ("comp", "Other Australian Competitive Grants"),
-    ("ncris", "NCRIS funding"),
-    ("ands_nectar_rds", "Funded by ARDC, Nectar, ANDS or RDS"),
     ("govt", "Other Australian government grant"),
     ("industry", "Industry funding"),
     ("ext", "Other external funding"),
