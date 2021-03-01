87b7db3fee2ac6d464d302183f3479a2fdf91bcd
Stephen Crawley <s.crawley@uq.edu.au>
2021-03-01 13:27:15 +1100

    Publications - DOI lookup, etc
      - Adds new fields 'output_type' and 'crossref_metadata'.
      - Adds browser-side DOI checker + prompting
      - Once a Publication has been accepted, it becomes effectively
        read or delete only and compact rendering is used to save
        screen space.
    
    TODO
    
      - More test cases
    
    FUTURE
    
      - Spinner for the DOI validator
      - No publications allowed for a new / pending-first-approval record.
      - Sanity checks; e.g. publication year >= year of first approval
    
    Change-Id: Ifed092e8fa943841441f6fc50e59e084032f6c30

---
+++ nectar_dashboard/rcallocation/tests/unit/test_templatetags.py
@@ -0,0 +1,137 @@
+# Copyright 2021 Australian Research Data Commons
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from openstack_dashboard.test import helpers
+
+from nectar_dashboard.rcallocation.templatetags import publication_extras
+
+
+class PublicationExtrasTestCase(helpers.TestCase):
+
+    def test_output_type_descr(self):
+        self.assertEqual("Other",
+                         publication_extras.output_type_descr('O'))
+        self.assertEqual("Unspecified",
+                         publication_extras.output_type_descr('U'))
+        self.assertEqual('',
+                         publication_extras.output_type_descr(''))
+        self.assertEqual("???",
+                         publication_extras.output_type_descr('Z'))
+
+    def test_crossref_summary(self):
+        self.assertEqual("*** No JSON ***",
+                         publication_extras.crossref_summary(""))
+        self.assertEqual("*** Invalid JSON ***",
+                         publication_extras.crossref_summary("Jason"))
+        self.assertEqual("*** Not a JSON object ***",
+                         publication_extras.crossref_summary('["Jason"]'))
+        self.assertEqual("*** Not a JSON Crossref response ***",
+                         publication_extras.crossref_summary(
+                             '{"name": "Jason"}'))
+
+        self.assertEqual("<i>Title</i>: Not recorded, "
+                         "<i>Author(s)</i>: Not recorded, "
+                         "<i>Publication</i>: Not recorded, "
+                         "<i>Year</i>: Not recorded",
+                         publication_extras.crossref_summary(
+                             '{"message": {"abc": 123}}'))
+
+        self.assertEqual("<i>Title</i>: one, "
+                         "<i>Author(s)</i>: Not recorded, "
+                         "<i>Publication</i>: Not recorded, "
+                         "<i>Year</i>: Not recorded",
+                         publication_extras.crossref_summary(
+                             '{"message": {"title": ["one", "two"]}}'))
+        # Prevent injection via the title
+        self.assertEqual("<i>Title</i>: &lt;i&gt;one&lt;/i&gt;, "
+                         "<i>Author(s)</i>: Not recorded, "
+                         "<i>Publication</i>: Not recorded, "
+                         "<i>Year</i>: Not recorded",
+                         publication_extras.crossref_summary(
+                             '{"message": {"title": ["<i>one</i>", "two"]}}'))
+
+        self.assertEqual("<i>Title</i>: Not recorded, "
+                         "<i>Author(s)</i>: Nurke,Fred, "
+                         "<i>Publication</i>: Not recorded, "
+                         "<i>Year</i>: Not recorded",
+                         publication_extras.crossref_summary(
+                             '{"message": {"author": [{"family": "Nurke",'
+                             '                         "given": "Fred"}]}}'))
+        self.assertEqual("<i>Title</i>: Not recorded, "
+                         "<i>Author(s)</i>: Nurke,Fred; Spriggs,Jim, "
+                         "<i>Publication</i>: Not recorded, "
+                         "<i>Year</i>: Not recorded",
+                         publication_extras.crossref_summary(
+                             '{"message": {"author": [{"family": "Nurke",'
+                             '                         "given": "Fred"},'
+                             '                        {"family": "Spriggs",'
+                             '                         "given": "Jim"}]}}'))
+        self.assertEqual("<i>Title</i>: Not recorded, "
+                         "<i>Author(s)</i>: Moriarty; Eccles, "
+                         "<i>Publication</i>: Not recorded, "
+                         "<i>Year</i>: Not recorded",
+                         publication_extras.crossref_summary(
+                             '{"message": '
+                             ' {"author": [{"family": "Moriarty"},'
+                             '             {"family": "Eccles"}]}}'))
+        # Prevent injection via the authors
+        self.assertEqual("<i>Title</i>: Not recorded, "
+                         "<i>Author(s)</i>: &lt;tag&gt;&amp;, "
+                         "<i>Publication</i>: Not recorded, "
+                         "<i>Year</i>: Not recorded",
+                         publication_extras.crossref_summary(
+                             '{"message": '
+                             ' {"author": [{"family": "<tag>&"}]}}'))
+
+        self.assertEqual("<i>Title</i>: Not recorded, "
+                         "<i>Author(s)</i>: Not recorded, "
+                         "<i>Publication</i>: Some Journal, "
+                         "<i>Year</i>: Not recorded",
+                         publication_extras.crossref_summary(
+                             '{"message": '
+                             '  {"container-title": ["Some Journal"]}}'))
+        # Prevent injection via the title
+        self.assertEqual("<i>Title</i>: Not recorded, "
+                         "<i>Author(s)</i>: Not recorded, "
+                         "<i>Publication</i>: &lt;beep&gt;, "
+                         "<i>Year</i>: Not recorded",
+                         publication_extras.crossref_summary(
+                             '{"message": '
+                             '  {"container-title": ["<beep>"]}}'))
+
+        self.assertEqual("<i>Title</i>: Not recorded, "
+                         "<i>Author(s)</i>: Not recorded, "
+                         "<i>Publication</i>: Not recorded, "
+                         "<i>Year</i>: 2021",
+                         publication_extras.crossref_summary(
+                             '{"message": '
+                             '  {"published-online": '
+                             '    {"date-parts": [[2021, 4, 1]]}}}'))
+        self.assertEqual("<i>Title</i>: Not recorded, "
+                         "<i>Author(s)</i>: Not recorded, "
+                         "<i>Publication</i>: Not recorded, "
+                         "<i>Year</i>: 2021",
+                         publication_extras.crossref_summary(
+                             '{"message": '
+                             '  {"published-print": '
+                             '    {"date-parts": [[2021, 4, 1]]}}}'))
+        # Prevent injection via the year
+        self.assertEqual("<i>Title</i>: Not recorded, "
+                         "<i>Author(s)</i>: Not recorded, "
+                         "<i>Publication</i>: Not recorded, "
+                         "<i>Year</i>: &lt;1066&gt;",
+                         publication_extras.crossref_summary(
+                             '{"message": '
+                             '  {"published-online": '
+                             '    {"date-parts": [["<1066>", 4, 1]]}}}'))
