73d5075027cb2f4ad94c2ff09d6819dc0d244a93
Stephen Crawley <s.crawley@uq.edu.au>
2021-05-14 15:57:47 +1000

    NCRIS facilities
      - Model changes
      - Migration to populate the table
      - Form and template changes (using django-select2 widget)
    
    Change-Id: Ieb9a937950aeed8b3c8e134cc045e1382fca54a3

--- nectar_dashboard/rcallocation/forms.py
+++ nectar_dashboard/rcallocation/forms.py
@@ -8,6 +8,8 @@ from django import forms
 from django.forms.forms import NON_FIELD_ERRORS
 from django.urls import reverse
 from django.utils.safestring import mark_safe
+
+from select2 import fields as select2_fields
 from select2 import forms as select2_forms
 
 from nectar_dashboard.rcallocation.forcodes import FOR_CODES
@@ -40,6 +42,12 @@ class UsageFieldWidget(forms.CheckboxSelectMultiple):
     option_template_name = 'rcallocation/usage_type.html'
 
 
+class NCRISChoiceField(select2_fields.ModelMultipleChoiceField):
+
+    def label_from_instance(self, facility):
+        return "%s - %s" % (facility.short_name, facility.name)
+
+
 class BaseAllocationForm(forms.ModelForm):
     error_css_class = 'has-error'
     ignore_warnings = forms.BooleanField(widget=forms.HiddenInput(),
@@ -47,6 +55,7 @@ class BaseAllocationForm(forms.ModelForm):
     field_of_research_1 = FoRChoiceField("First Field Of Research")
     field_of_research_2 = FoRChoiceField("Second Field Of Research")
     field_of_research_3 = FoRChoiceField("Third Field Of Research")
+
     usage_types = forms.ModelMultipleChoiceField(
         help_text="""Select one or more items that best describe what
                      you are using the Nectar Research Cloud for.  If
@@ -58,12 +67,30 @@ class BaseAllocationForm(forms.ModelForm):
         widget=UsageFieldWidget(attrs={'class': 'form-inline list-unstyled'}),
         to_field_name='name')
 
+    ncris_facilities = NCRISChoiceField(
+        name='ncris_facilities',
+        model=models.NCRISFacility,
+        label="NCRIS facilities supporting this request",
+        help_text="""Select NCRIS facilities where the facility management
+                     actively supports this request in furtherance of its
+                     goals.  For example, the requested resources may
+                     enable a project that the NCRIS facility is funding,
+                     or they may enable the provision of infrastructure
+                     for the facility.
+        """,
+        required=False,
+        queryset=models.NCRISFacility.objects.all(),
+#        widget=FilteredSelectMultiple('NCRIS Facilities', is_stacked=False,
+#                                      attrs={'class': 'col-md-6'}),
+        to_field_name='short_name')
+
     class Meta:
         model = models.AllocationRequest
         exclude = ('status', 'created_by', 'submit_date', 'approver_email',
                    'start_date', 'end_date', 'modified_time', 'parent_request',
                    'associated_site', 'provisioned', 'managed',
-                   'project_id', 'notes', 'notifications')
+                   'project_id', 'notes', 'notifications', 'ncris_support'
+        )
 
         widgets = {
             'status_explanation': forms.Textarea(
@@ -96,7 +123,7 @@ class BaseAllocationForm(forms.ModelForm):
             'for_percentage_2': forms.Select(attrs={'class': 'col-md-2'}),
             'for_percentage_3': forms.Select(attrs={'class': 'col-md-2'}),
             'nectar_support': forms.TextInput(attrs={'class': 'col-md-12'}),
-            'ncris_support': forms.TextInput(attrs={'class': 'col-md-12'}),
+            'ncris_explanation': forms.TextInput(attrs={'class': 'col-md-12'}),
         }
 
     groups = (
