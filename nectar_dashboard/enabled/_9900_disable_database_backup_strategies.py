0d5d92cf211bea675cdc9bbf69d688948535babb
Sam Morrison <sorrison@gmail.com>
2021-10-07 11:28:35 +1100

    Disable database backup strategies panel
    Change-Id: Ie476b342d1e794850d803ab4d8a500e007e4e740

---
+++ nectar_dashboard/enabled/_9900_disable_database_backup_strategies.py
@@ -0,0 +1,7 @@
+PANEL = 'backup_strategies'
+# The slug of the dashboard the PANEL associated with. Required.
+PANEL_DASHBOARD = 'project'
+# The slug of the panel group the PANEL is associated with.
+PANEL_GROUP = 'database'
+
+REMOVE_PANEL = True
