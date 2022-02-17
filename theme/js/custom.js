aec4afee393aeb149c4f9dda0b1dfff62a59f40a
Darcelle Malby <d.malby@uq.edu.au>
2022-02-17 16:02:19 +1100

    Initial theme changes and new login page
    Change-Id: I009535242c60bb8c6cac18aae44de18b628bced8

---
+++ theme/js/custom.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  // Add slideDown animation to Bootstrap dropdown when expanding.
+  $('.dropdown').on('show.bs.dropdown', function() {
+    $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
+  });
+
+  // Add slideUp animation to Bootstrap dropdown when collapsing.
+  $('.dropdown').on('hide.bs.dropdown', function() {
+    $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
+  });
+});
