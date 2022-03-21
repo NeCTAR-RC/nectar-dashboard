42c5a6c8448867bc214d6065ee0f367b11734fff
Sam Morrison <sorrison@gmail.com>
2022-03-21 15:31:36 +1100

    Add more config that should be in code
    To simplify config move some more stuff that is more
    related to code options than things we need to configure
    in a config file.
    
    Change-Id: I6d7099d53144049f61e6064d11d9ed2ef91e13dd

--- nectar_dashboard/local_settings.d/_50_nectar.py
+++ nectar_dashboard/local_settings.d/_50_nectar.py
@@ -4,3 +4,51 @@ ADD_INSTALLED_APPS = [
     'corsheaders',
     'select2',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS':
+    'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 1000,
+    'DEFAULT_FILTER_BACKENDS':
+    ('django_filters.rest_framework.DjangoFilterBackend',),
+    'DEFAULT_AUTHENTICATION_CLASSES':
+    (
+        'nectar_dashboard.rest_auth.CsrfExemptSessionAuthentication',
+        'nectar_dashboard.rest_auth.KeystoneAuthentication',
+    ),
+}
+
+REST_VIEW_SETS = (
+    ('allocations',
+     'nectar_dashboard.rcallocation.api.AllocationViewSet', None),
+    ('quotas',
+     'nectar_dashboard.rcallocation.api.QuotaViewSet', None),
+    ('chiefinvestigators',
+     'nectar_dashboard.rcallocation.api.ChiefInvestigatorViewSet', None),
+    ('institutions',
+     'nectar_dashboard.rcallocation.api.InstitutionViewSet', None),
+    ('publications',
+     'nectar_dashboard.rcallocation.api.PublicationViewSet', None),
+    ('grants',
+     'nectar_dashboard.rcallocation.api.GrantViewSet', None),
+    ('resources',
+     'nectar_dashboard.rcallocation.api.ResourceViewSet', None),
+    ('zones',
+     'nectar_dashboard.rcallocation.api.ZoneViewSet', None),
+    ('service-types',
+     'nectar_dashboard.rcallocation.api.ServiceTypeViewSet', None),
+    ('ncris-facilities',
+     'nectar_dashboard.rcallocation.api.NCRISFacilityViewSet', None),
+    ('ardc-projects',
+     'nectar_dashboard.rcallocation.api.ARDCSupportViewSet', None),
+    ('sites',
+     'nectar_dashboard.rcallocation.api.SiteViewSet', None),
+    ('approvers',
+     'nectar_dashboard.rcallocation.api.ApproverViewSet', None),
+    ('for-codes',
+     'nectar_dashboard.rcallocation.api.for.FORViewSet',
+     'for-codes'),
+    ('for-tree',
+     'nectar_dashboard.rcallocation.api.for.AllocationTreeViewSet',
+     'for-tree'),
+)
