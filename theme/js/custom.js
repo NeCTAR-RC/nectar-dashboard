c4e3359686cc7e0098b1293f6b297b38f66de54b
Darcelle Malby <d.malby@uq.edu.au>
2022-11-03 14:37:49 +1100

    Stopping login page animations after 1 minute
    Change-Id: I67ec228fad40b69e9cb817f9e18247cf93e27d08

--- theme/js/custom.js
+++ theme/js/custom.js
@@ -8,6 +8,18 @@ $(document).ready(function() {
   $('.dropdown').on('hide.bs.dropdown', function() {
     $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
   });
+
   // Activate Bootstrap 3 tooltips
   $('[data-toggle="tooltip"]').tooltip();
+
+  // Is it the login page?
+  if($("#splash")) {
+    // Stop css animations when
+    setTimeout(function(){
+      $("#hex-flashing polygon").addClass("paused");
+      $("#lines path").addClass("paused");
+      $("#lines line").addClass("paused");
+    }, 60000);
+  }
+
 });
