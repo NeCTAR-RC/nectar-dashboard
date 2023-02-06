ec7f74f798545c4ef738692eee15dab238923835
Stephen Crawley <s.crawley@uq.edu.au>
2023-02-06 17:23:30 +1000

    Add user's first name to landing page
    Change-Id: I966e887cf986637dc04fc896c6a7f7458e2617ab

--- nectar_dashboard/dashboard_home/welcome/views.py
+++ nectar_dashboard/dashboard_home/welcome/views.py
@@ -2,11 +2,29 @@ from django.http import HttpResponse
 from horizon import views as horizon_views
 import requests
 
+from nectar_dashboard.api import manuka
+
 
 class HomeView(horizon_views.HorizonTemplateView):
 
     template_name = 'dashboard_home/home.html'
     page_title = "Home"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        try:
+            keystone_id = self.request.user.keystone_user_id
+            client = manuka.manukaclient(self.request)
+            self.manuka_user = client.users.get(keystone_id)
+            context['first_name'] = self.manuka_user.first_name
+            context['surname'] = self.manuka_user.surname
+            context['displayname'] = self.manuka_user.displayname
+        except Exception:
+            context['first_name'] = ""
+            context['surname'] = ""
+            context['displayname'] = ""
+
+        return context
 
 
 def get_ardc_news(request):
