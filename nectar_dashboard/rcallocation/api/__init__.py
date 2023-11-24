7b3f7201923d7195a110ac6007398f513ce82293
Stephen Crawley <s.crawley@uq.edu.au>
2023-11-24 11:46:43 +1000

    Add filters to the Organisations API
    Change-Id: Idda175b4acec54d215334f5046f5af36131dfb1c

--- nectar_dashboard/rcallocation/api/__init__.py
+++ nectar_dashboard/rcallocation/api/__init__.py
@@ -185,9 +185,23 @@ class ProposedOrganisationSerializer(CountryFieldMixin,
         exclude = ['vetted_by', 'proposed_by']
 
 
+class OrganisationFilter(filters.FilterSet):
+
+    class Meta:
+        model = models.Organisation
+
+        fields = {'ror_id': ['exact', 'iexact'],
+                  'short_name': ['exact', 'iexact'],
+                  'full_name': ['exact', 'iexact', 'icontains'],
+                  'country': ['exact', 'iexact'],
+                  'url': ['exact', 'iexact'],
+        }
+
+
 class OrganisationViewSet(PermissionMixin, NoDestroyViewSet):
     queryset = models.Organisation.objects.all()
     serializer_class = OrganisationSerializer
+    filter_class = OrganisationFilter
 
     def get_serializer_class(self):
         if self.is_read_admin():
