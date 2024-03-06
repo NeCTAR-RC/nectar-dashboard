a7cc749d7d0e71c1fde1d31ea7110227afa12d63
Sam Morrison <sorrison@gmail.com>
2024-03-06 17:07:18 +1100

    Code style import modules not classes
    Change-Id: I4558eca1e66c11e78a90dab1b602c08652145dfa

--- nectar_dashboard/rcallocation/user_allocations/urls.py
+++ nectar_dashboard/rcallocation/user_allocations/urls.py
@@ -1,21 +1,21 @@
 from django.urls import re_path
 
-from nectar_dashboard.rcallocation.user_allocations.views import RestrictedAllocationsEditView  # noqa
-from nectar_dashboard.rcallocation.user_allocations.views import RestrictedAllocationsDetailsView  # noqa
-from nectar_dashboard.rcallocation.user_allocations.views import UserAllocationsListView  # noqa
-from nectar_dashboard.rcallocation.user_allocations.forms import UserAllocationRequestAmendForm  # noqa
+from nectar_dashboard.rcallocation.user_allocations import forms
+from nectar_dashboard.rcallocation.user_allocations import views
+
 
 urlpatterns = [
     re_path(r'^$',
-        UserAllocationsListView.as_view(), name='index'),
-    re_path(r'^view/(?P<pk>\d+)/$', RestrictedAllocationsDetailsView.as_view(),
+        views.UserAllocationsListView.as_view(), name='index'),
+    re_path(r'^view/(?P<pk>\d+)/$',
+            views.RestrictedAllocationsDetailsView.as_view(),
         name='allocation_view'),
     re_path(r'^edit_request/(?P<pk>\d+)/$',
-        RestrictedAllocationsEditView.as_view(),
+        views.RestrictedAllocationsEditView.as_view(),
         name='edit_request'),
     re_path(r'^edit_change_request/(?P<pk>\d+)/$',
-        RestrictedAllocationsEditView.as_view(
-            form_class=UserAllocationRequestAmendForm,
+        views.RestrictedAllocationsEditView.as_view(
+            form_class=forms.UserAllocationRequestAmendForm,
             page_title='Request Allocation Extension',
             template_name='rcallocation/allocationrequest_extend.html'),
         name='edit_change_request'),
